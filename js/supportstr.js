c0e9b6df083dea08cc75aefc44afe2a651beee07
wdingsoft <wdingbox@gmail.com>
2021-01-26 12:43:29 -0500

    update footoe

---
+++ js/supportstr.js
@@ -0,0 +1,16 @@
+var supportstr =`
+
+    <ol>To support and improve this group.
+    <li>
+    to use it and to give feedback.
+    </li>
+    <li>
+    to join our tech team to improve the codes.
+    </li>
+    <li>
+    to donate financially to maintain the service fee.
+    </li>
+
+
+    </ol>
+`
---
+++ js/techfeaturestr.js
@@ -0,0 +1,20 @@
+var techfeaturestr =`
+
+<ol>
+Uesage Features:
+    <li>Quick positioning of any verse (QPV).</li>
+    <li>Take a note of verse.</li>
+    <li>Search any word in verse.</li>
+    <li>Link external resouces for verse.</li>
+</ol>
+
+<ol>
+Design Features:
+    <li>The biblical-oriented app tool: the walking stick ( (Hebrew: מַטֶּה‎ matteh, translated "rod" in the King James Bible) of Moses.</li>
+    <li>Account free: We do not own or maintain any user account. We make use of the github account. Users own their data (UOD) </li>
+    <li>No installation is required.</li>
+    <li>Crossnetwork sharing platform: provide a way to share notes with certain rules.</li>
+    <li>Secure: creditial users info is fully encrypted across network and server.</li>
+</ol>
+
+`
