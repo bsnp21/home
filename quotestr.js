25eabbadfbf7577fca68fda8ddee68b5755161d6
wdingsoft <wdingbox@gmail.com>
2021-01-26 09:31:27 -0500

    css

--- quotestr.js
+++ quotestr.js
@@ -5,26 +5,29 @@ var quotestr =`
 			reading and studying the Bible involves two additional activities: asking questions of the text and
 			writing down
 			your insight. You haven't really studied the Bible unless you've written your thoughts down on paper or
-			comupter.</q> --Rick Warren (<cite>The Purpose Driven Life</cite>, day24, p187)
+			comupter.</q> 
+			<br><a class='smallnote'>-- Rick Warren (<cite>The Purpose Driven Life</cite>, day24, p187)</a>
 	</blockquote>
 
 	<blockquote class='centeredtxt'>
 		<q>The best way to become a 'doer of the Word' is to always write out an action step as a result of your
 			reading or
 			studying or reflecting on God's Word. Develop the habit of writing down exactly what you intend to
-			do.</q> --Rick
-		Warren (<cite>The Purpose Driven Life</cite>, day24, p190)
+			do.</q> 
+			<br><a class='smallnote'>-- Rick Warren (<cite>The Purpose Driven Life</cite>, day24, p190)</a>
 	</blockquote>
 
 	<blockquote class='centeredtxt'>
-		<q reference='The Purpose Driven Life, day24, p190'>The Bible was not given to increase our knowledge but to change our lives.</q> -- D.L. Moody
+		<q reference='The Purpose Driven Life, day24, p190'>The Bible was not given to increase our knowledge but to change our lives.</q> <br><a class='smallnote'>-- D.L. Moody</a>
 	</blockquote>
 
 	<blockquote class='centeredtxt'>
-		<q>Many people have a good habit of writing down a dairy. But it's usually a date based, ordered and centered rather than the Word centered. It would soon become meaningless & usesless. Only the biblical centered notes will shine forever. It is a way to store treasures in Heaven.(Mat 6:20)</q> -- William Wei Ding.
+		<q>Many people have a good habit of writing down a dairy. But it's usually a date based, ordered and centered rather than the Word centered. It would soon become meaningless & usesless. Only the biblical centered notes will shine forever. It is a way to store treasures in Heaven.(Mat 6:20)</q> 
+		<br><a class='smallnote'>-- William Wei Ding.</a>
 	</blockquote>
 
 	<blockquote class='centeredtxt'>
-		"Your word is a lamp to my feet and a light for my path." (Psm119:105 NIV)
+		"Your word is a lamp to my feet and a light for my path." 
+		<br><a class='smallnote'>(Psm119:105 NIV)</a>
 	</blockquote>
 `
