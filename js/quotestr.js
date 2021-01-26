4ba856da8bd17377568ed1a113f164ef4ef0c7f9
wdingsoft <wdingbox@gmail.com>
2021-01-26 09:44:09 -0500

    reorg

---
+++ js/quotestr.js
@@ -0,0 +1,33 @@
+var quotestr =`
+
+	<blockquote class='centeredtxt'>
+		<q>Researching, or <b>studying</b>, the Bible is another practical way to abide in the Word. The difference between
+			reading and studying the Bible involves two additional activities: asking questions of the text and
+			writing down
+			your insight. You haven't really studied the Bible unless you've written your thoughts down on paper or
+			comupter.</q> 
+			<br><a class='smallnote'>-- Rick Warren (<cite>The Purpose Driven Life</cite>, day24, p187)</a>
+	</blockquote>
+
+	<blockquote class='centeredtxt'>
+		<q>The best way to become a 'doer of the Word' is to always write out an action step as a result of your
+			reading or
+			studying or reflecting on God's Word. Develop the habit of writing down exactly what you intend to
+			do.</q> 
+			<br><a class='smallnote'>-- Rick Warren (<cite>The Purpose Driven Life</cite>, day24, p190)</a>
+	</blockquote>
+
+	<blockquote class='centeredtxt'>
+		<q reference='The Purpose Driven Life, day24, p190'>The Bible was not given to increase our knowledge but to change our lives.</q> <br><a class='smallnote'>-- D.L. Moody</a>
+	</blockquote>
+
+	<blockquote class='centeredtxt'>
+		<q>Many people have a good habit of writing down a dairy. But it's usually a date based, ordered and centered rather than the Word centered. It would soon become meaningless & usesless. Only the biblical centered notes will shine forever. It is a way to store treasures in Heaven.(Mat 6:20)</q> 
+		<br><a class='smallnote'>-- William Wei Ding.</a>
+	</blockquote>
+
+	<blockquote class='centeredtxt'>
+		"Your word is a lamp to my feet and a light for my path." 
+		<br><a class='smallnote'>(Psm119:105 NIV)</a>
+	</blockquote>
+`
