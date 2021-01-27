42ae73adc94d60a869ed748d6f32b903e180e901
wdingsoft <wdingbox@gmail.com>
2021-01-26 22:51:36 -0500

    upate tech

--- js/techfeaturestr.js
+++ js/techfeaturestr.js
@@ -1,38 +1,60 @@
 var techfeaturestr =`
 
 <ol>
-Usage Features:
-    <li>Fast locate any verse of the Bible.</li>
-    <li>Take a note on any verse of the Bible.</li>
-    <li>Search any word in different versions of Bibles.</li>
-    <li>Link external resouces for any verse of the Bible.</li>
-    <li>Share personal notes online for any verse of the Bible.</li>
+The Bible study notes are classified into three categories and stored in three text files in user's repository respectively:
+    <table border='1'>
+    <caption></caption>
+    <thead>
+    <tr><th>#</th><th>Type of Notes(files)</th><th>Description</th></tr>
+    </thead>
+    <tbody>
+    <tr><td>1</td><td>_myCrossReferences</td><td>for related verses in the Bible. Recommended to follow the standard abbreviation format. E.g. 
+    <br>Gen2:7 refers to Genesis, chapter 2, verse 7.
+    <br>1Jn5:12 refers to 1-John, chapter 5, verse 12.</td></tr>
+
+    <tr><td>2</td><td>_myNotes</td><td>for commentary, questions, testimony, reflections, takeaway, other website links etc.</td></tr>
+
+    <tr><td>3</td><td>_myPrayer</td><td>for the prayer related to the verse and real life..</td></tr>
+    </tbody>
+    </table>
+    * The maximum size of each note-file is limited to 100 (MB).<br>
+    * Recommended size of each piece of note for each verse is one page (about 3,000 characters, 500 words)<br>
+    * There are total 31,102 verses in the Bible.
 </ol>
 
 <ol>
+To facilitate quickly writing note on any verse in the Bible, the app  
+    <li>utilize the First-Capital Keyboard method, category method, Most-Recent-History(MRH) methods.</li>
+    <li>allows to search any word in different versions of Bibles.</li>
+    <li>provides predefined external web links for each verse.</li>
+    <li>allows to share with other people's notes online.</li>
+</ol>
+
+
+<ol>
 Design Features:
-    <li>Biblical-oriented design: The program is biblical structured, implimented and operated.</li>
-    <li>On-line app. No installation; No Ad; No virus; No hassels.</li>
-    <li>Account free: The online app do not own any user account. (The user account belongs to the github) </li>
-    <li>Crossnetwork sharing platform: Provide a safe way to share notes with certain rules.</li>
+    <li>Biblical-oriented design: The program is biblical structured, implemented and operated.</li>
+    <li>On-line app. No installation; No Ad; No virus; No hassle.</li>
+    <li>Account free: The app does not own any user account. (The user account belongs to the github) </li>
+    <li>Cross-network sharing: Provide a safe way to share notes with certain rules.</li>
     <li>The 4096-bit RSA standard encryption: Users confidential info is fully encrypted across network and server site. (bank-level security)</li>
-    <li>Open sources development enviroment: The source codes are free to download and install on any server.</li>
+    <li>Open sources development environment: The source codes are free to download and install on any server.</li>
 </ol>
 <ol>
     Development Principles
     <li>
-    Maximized Quality & minimized cost.
+    Maximized quality & minimized cost.
     </li>
     <li>
-    Simpliest & saftest popular mature clean technology.
+    Simplest & saftest popular mature clean technology.
     </li>
 </ol>
 <ol>
-    Application Goal
-    <li>To provide a high quality convenient Bible study tool: A good biblical study tool is like the walking stick ( (Hebrew: מַטֶּה‎ matteh, translated "rod" in the King James Bible) of Moses.
+    Application Goals
+    <li>To provide a high-quality convenient Bible study tool for Christians, which is powerful as if it were like the walking stick (Hebrew: מַטֶּה‎ matteh, translated "rod" in the King James Bible) of Moses.
     </li>
     <li>
-    To use advanced technology to serve and glorify for Him rather than any culture, race and kingdim.
+    To use advanced technology to serve and glorify Him rather than any idol, culture, race and worldly state.
     </li>
 </ol>
 
