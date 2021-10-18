d589f6efa84b793fa2a45a05a503310764b716d3
wdingsoft <wdingbox@gmail.com>
2021-10-18 13:35:47 -0400

    obilib_work_path=

--- js/config_websrv_data.js
+++ js/config_websrv_data.js
@@ -1,7 +1,8 @@
-var config_websvr_data = {
+const config_websvr_data = {
     ip: "34.227.20.213",
     http_port: "7778",
     https_port:"7775",
     mySignIn_path_file:"wdaws/bb/np/mySignIn.htm",
-    
+
+    obilib_work_path:"obi/Lab/obilab/test1/oohtm/works1"
 }
