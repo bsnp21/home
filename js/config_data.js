dceb7c0769d03112a62d5cb644d0603946b740db
wdingsoft <wdingbox@gmail.com>
2021-10-18 13:31:57 -0400

    change to name config_websrv_data.js

--- js/config_data.js
+++
@@ -1,6 +0,0 @@
-var config_data = {
-    ip: "34.227.20.213",
-    http_port: "7778",
-    https_port:"7775",
-    web_path:"wdaws/bb/np/mySignIn.htm",
-}
---
+++ js/config_websrv_data.js
@@ -0,0 +1,7 @@
+var config_websvr_data = {
+    ip: "34.227.20.213",
+    http_port: "7778",
+    https_port:"7775",
+    mySignIn_path_file:"wdaws/bb/np/mySignIn.htm",
+    
+}
