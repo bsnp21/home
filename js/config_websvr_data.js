e3636a7885f5313d679de7798323d22e4f685ed7
wdingsoft <wdingbox@gmail.com>
2022-11-14 07:21:44 -0500

    remove samp

--- js/config_websvr_data.js
+++ js/config_websvr_data.js
@@ -11,9 +11,4 @@ var config_websvr_data = {
     obilib_work_path: "obi/Lab/obilab/test1/oohtm/works1"
 }
 
-var Samples_Guests_Info = {
 
-    "guest01": { rep: "https://wdingsoft@bitbucket.org/bsnp21/guest_01.git", pat: "gSzkTPgF" + "Nnz9v5wGF5eN", opt: "" },
-    "guest02": { rep: "https://wdingsoft@bitbucket.org/bsnp21/guest_02.git", pat: "gSzkTPgF" + "Nnz9v5wGF5eN", opt: "run" }
-
-}
