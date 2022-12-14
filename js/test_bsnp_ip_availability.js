62b10ff92e1f0013ab532e8ac754cd837c1c5a14
wdingsoft <wdingbox@gmail.com>
2022-12-14 13:15:00 -0500

    better home

--- js/test_bsnp_ip_availability.js
+++ js/test_bsnp_ip_availability.js
@@ -9,50 +9,57 @@ function gen_test_ip_availability() {
         // svr port https or http. 
         var protocal = $("#host_cap .protocal.hili").text()
         var port = BsnpSvc_IP_Info.Ports.host
-        var Hostary = ["./api_demo_local.htm","../np/mySignIn.htm", "https://bsnp21.github.io/bb/np/mySignIn.htm"];
+        var Hostary = ["./api_demo_local.htm", "../np/mySignIn.htm", "https://bsnp21.github.io/bb/np/mySignIn.htm"];
         Object.keys(BsnpSvc_IP_Info.IP).forEach(function (ip) {
             Hostary.push(`${protocal}://${ip}:${port[protocal]}/wdaws/bb/np/mySignIn.htm`)
         })
         console.log(Hostary)
         var str = ""
         Hostary.forEach(function (key, i) {
-            var default_hili = (3 === i) ? " hili" : ""
-            str += `<tr><td>${i}</td><td class='host${default_hili}'>${key}</td></tr>`
+            var host_storeIdx = localStorage.getItem("host_storeIdx")
+            if (!host_storeIdx) host_storeIdx = 3
+            else { host_storeIdx = parseInt(host_storeIdx) }
+            var default_hili = (host_storeIdx === i) ? " hili" : ""
+            str += `<tr><td>${i}</td><td host_storeIdx='${i}' class='host ${default_hili}'>${key}</td></tr>`
         })
         $("#hostaddr").html(str)
         $(".host").on("click", function () {
             $(".host.hili").removeClass("hili")
             $(this).addClass("hili")
             _THIS.gen_linker()
+            localStorage.setItem("host_storeIdx", $(this).attr("host_storeIdx"))
         })
     }
     function gen_svc() {
         var protocal = $("#svr_cap .protocal.hili").text().trim()
-        var str = "<tr><td>-</td></tr><tr><td>-</td></tr>"
+        var str = "<tr><td>-</td></tr><tr><td>-</td></tr><tr><td>-</td></tr>"
         Object.keys(BsnpSvc_IP_Info.IP).forEach(function (ip, i) {
             var url = `${protocal}://${ip}:${BsnpSvc_IP_Info.Ports.bsnpa[protocal]}`
             var portc = BsnpSvc_IP_Info.Ports.bsnpc[protocal]
             var url_c = `${protocal}://${ip}:${portc}`
             var par = `?sip=${url}`
-            var default_hili = (0 === i) ? " hili" : ""
-            str += `<tr><td class='svc${default_hili}' ip='${ip}'>${par}</td><td><a href='${url}'>/</a></td><td><a href='${url}/get_OTK'>get_OTK</a></td><td><a href='${url_c}'>${portc}</a></td></tr>`
+            var svc_storeIdx = localStorage.getItem("svc_storeIdx")
+            if (!svc_storeIdx) svc_storeIdx = 0
+            else { svc_storeIdx = parseInt(svc_storeIdx) }
+            var default_hili = (svc_storeIdx === i) ? " hili" : ""
+            str += `<tr><td svc_storeIdx='${i}' class='svc${default_hili}' ip='${ip}'>${par}</td><td><a href='${url}'>/</a></td><td><a href='${url}/get_OTK'>get_OTK</a></td><td><a href='${url_c}'>${portc}</a></td></tr>`
         })
         $("#svcaddr").html(str)
         $(".svc").on("click", function () {
             $(".svc.hili").removeClass("hili")
             $(this).addClass("hili")
             _THIS.gen_linker()
-
+            localStorage.setItem("svc_storeIdx", $(this).attr("svc_storeIdx"))
         })
     }
 
 
-   
+
 
     $(function () {
         gen_host()
         gen_svc()
-        
+
         $("#svr_cap .protocal").on("click", function () {
             $("#svr_cap .protocal.hili").removeClass("hili")
             $(this).addClass("hili")
@@ -71,17 +78,19 @@ function gen_test_ip_availability() {
     })
 }
 gen_test_ip_availability.prototype.guest_auto_login = function () {
-   
+
     var par = `&guest_auto_login=1`
     return par
 }
 gen_test_ip_availability.prototype.gen_linker = function (i) {
     var str = $(".host.hili").text()
     str += $(".svc.hili").text()
-    
-    $("#linkstr").text(str )
-    $("#linkstr").attr("href", str )
-    console.log("linker=",str)
+
+    $("#linkstr").text(str)
+    $("#linkstr").attr("href", str)
+    console.log("linker=", str)
+    localStorage.setItem("users_signin_page_url", str)
+    localStorage.setItem("guest_signin_page_url", str + this.guest_auto_login())
     return str
 }
 $(function () {
