5c6b74db4f9fdbc4058ed7e66ba5ed7d533fb416
wdingsoft <wdingbox@gmail.com>
2025-02-24 09:17:30 -0500

    update home index

--- js/config_websvr_data.js
+++ js/config_websvr_data.js
@@ -2,13 +2,20 @@ var config_websvr_data = {
     ip: "34.224.88.91",
     http_port: "7778",
     https_port: "7775",
-    mySignIn_path_file: "wdaws/bb/np/mySignIn.htm",
-
-
-    //https://bsnp21.github.io/bb/np/mySignIn.htm?sip=https://34.227.20.213:7775
-    github_host_signin_url: "https://bsnp21.github.io/bb/np/mySignIn.htm", //?sip=https://34.227.20.213:7775",
-
-    obilib_work_path: "obi/Lab/obilab/test1/oohtm/works1"
 }
 
 
+
+config_websvr_data.mySignIn_url = `http://${config_websvr_data.ip}/wdaws/bb/np/mySignIn.htm?sip=http://${config_websvr_data.ip}:7778`
+
+
+
+/////
+//reminder
+/////
+config_websvr_data.mySignIn_path_file = "wdaws/bb/np/mySignIn.htm"
+
+//https://bsnp21.github.io/bb/np/mySignIn.htm?sip=https://34.227.20.213:7775
+config_websvr_data.github_host_signin_url = "https://bsnp21.github.io/bb/np/mySignIn.htm" //?sip=https://34.227.20.213:7775",
+
+config_websvr_data.obilib_work_path = "obi/Lab/obilab/test1/oohtm/works1"
