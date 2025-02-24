a1bf1c605ad68a69356d2aa074d2b8810a06abfe
wdingsoft <wdingbox@gmail.com>
2025-02-24 02:40:49 -0500

    '1.1.1.1'

---
+++ js/InstalledDeviceIP.js
@@ -0,0 +1,1 @@
+var InstalledDeviceIP = '1.1.1.1'
---
+++ js/InstalledDeviceIP_set.sh
@@ -0,0 +1,25 @@
+
+function Generate_IP_Config_File(){
+   
+    #dig +short myip.opendns.com @resolver1.opendns.com 
+    SvrIP=`dig +short myip.opendns.com @resolver1.opendns.com`
+    echo SvrIP=${SvrIP}
+
+    if [ ! -z "${SvrIP}" -a "${SvrIP}" == "" ]; then
+        echo "SvrIP is empty!"
+        exit
+    fi
+}
+
+ Generate_IP_Config_File 
+
+ if [ ! -z "$1" -a "$1" == "" ]; then
+        echo "SvrIP is empty!"
+        exit
+    fi
+
+FileName="InstalledDeviceIP.js"
+echo "var InstalledDeviceIP = '$1'" > ${FileName}
+git add *
+git commit -m "'$1'"
+git push
