42c4c1c50584fb75483ffb500c13e337f03ed1cc
wdingsoft <wdingbox@gmail.com>
2025-02-24 03:38:49 -0500

    a

--- js/InstalledDeviceIP_set.sh
+++ js/InstalledDeviceIP_set.sh
@@ -2,10 +2,10 @@
 
 
 
-git clone https://github.com/bsnp21/home.git
+#git clone https://github.com/bsnp21/home.git
 
 FileName="InstalledDeviceIP.js"
 echo " InstalledDeviceIP = '$1'" > ${FileName}
-git add *
-git commit -m "'$1'"
-git push
+# git add *
+# git commit -m "'$1'"
+# git push
